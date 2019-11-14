1e257ab9ce99e3568b4fb03a5f40054fe3bae980
Oliver <gymi.mine@gmail.com>
2019-11-14 21:16:05 +0100

    Homework week 46 - Wrote factorial function - Wrote permutation function - Wrote combinations function

---
+++ combinations.py
@@ -0,0 +1,23 @@
+from factorial import factorial
+
+def combinations(n, k):
+    C = (factorial(n) / (factorial(k) * factorial(n - k)))
+    return C
+
+
+high_number = -1
+low_number = -1
+
+print("C(n,k) = n! / (k*(n - k)!)\n" + "n has to be greater than or equal to k")
+
+while high_number < 1 or high_number < 0:
+    high_number = int(input("n= "))
+    if high_number < 1:
+        print("n must be greater than or equal to 1")
+
+while low_number > high_number or low_number < 0:
+    low_number = int(input("k= "))
+    if low_number > high_number:
+        print("k must be smaller than or equal to n")
+
+print("C(" + str(high_number) + "," + str(low_number) + ") = " + str(combinations(high_number, low_number)))
---
+++ factorial.py
@@ -0,0 +1,11 @@
+def factorial(number):
+    if number == 0:
+        return 1
+    elif number < 0:
+        print ("ERROR negative number entered")
+        return
+    else:
+        product = 1
+        for i in range(0, number):
+            product = product * (number - i)
+    return product
